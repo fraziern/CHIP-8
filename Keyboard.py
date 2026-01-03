29a16f8221e3952979d839791c684096c3bbe631
Nicholas Frazier <nrflaw@gmail.com>
2026-01-03 08:38:51 -0500

    save kbd state

--- Keyboard.py
+++ Keyboard.py
@@ -3,56 +3,74 @@ import pygame
 
 class Keyboard():
 
-    keys = { 0:pygame.K_x,
-        1:pygame.K_1,
-        2:pygame.K_2,
-        3:pygame.K_3,
-        4:pygame.K_q,
-        5:pygame.K_w,
-        6:pygame.K_e,
-        7:pygame.K_a,
-        8:pygame.K_s,
-        9:pygame.K_d,
-        0xa:pygame.K_z,
-        0xb:pygame.K_c,
-        0xc:pygame.K_4,
-        0xd:pygame.K_r,
-        0xe:pygame.K_f,
-        0xf:pygame.K_v,
+    keytable = { pygame.K_x:0,
+        pygame.K_1:1,
+        pygame.K_2:2,
+        pygame.K_3:3,
+        pygame.K_q:4,
+        pygame.K_w:5,
+        pygame.K_e:6,
+        pygame.K_a:7,
+        pygame.K_s:8,
+        pygame.K_d:9,
+        pygame.K_z:0xa,
+        pygame.K_c:0xb,
+        pygame.K_4:0xc,
+        pygame.K_r:0xd,
+        pygame.K_f:0xe,
+        pygame.K_v:0xf,
         }
+
 
     def __init__(self):
         pygame.init()
+        # current_state and previous_state hold true/false for each key (16 total)
+        self.previous_state = [False] * 16
+        self.current_state = [False] * 16
+        self.request_quit = False
     
 
-    def get_events(self):
-        actions = {'quit':False,'keydown':False}
+    # def get_events(self):
+    #     actions = {'quit':False,'keydown':False}
+
+    #     for event in pygame.event.get():
+    #         print(event)
+    #         # Check for Window Close (X button)
+    #         if event.type == pygame.QUIT:
+    #             actions['quit'] = True
+                
+    #         # Check for  Key Press
+    #         if event.type == pygame.KEYDOWN and event.key in self.keytable.keys():
+    #             found_key = self.keytable[event.key]
+    #             actions[found_key] = True
+    #             actions['keydown'] = True
+                    
+    #     return actions
+    
+
+    def get_state(self):
+        # print(self.current_state, end="\r", flush=True)
+        self.previous_state = self.current_state.copy()
 
         for event in pygame.event.get():
-            # Check for Window Close (X button)
             if event.type == pygame.QUIT:
-                actions['quit'] = True
-                
-            # Check for  Key Press
-            if event.type == pygame.KEYDOWN and event.key in self.keys.values():
-                found_key = next((key for key, value in self.keys.items() if value == event.key), None)
-                actions[found_key] = True
-                actions['keydown'] = True
-                    
-        return actions
-    
+                self.request_quit = True
+            if event.type == pygame.KEYDOWN and event.key in self.keytable.keys():
+                self.current_state[self.keytable[event.key]] = True
+            elif event.type == pygame.KEYUP and event.key in self.keytable.keys():
+                self.current_state[self.keytable[event.key]] = False
+        
     
     def is_pressed(self, key_hex:int = None):
+        # this does not get new events, but only checks current/previous state
 
         #1. individual actions
         if key_hex is None:
-            for event in pygame.event.get():
-                if event.type == pygame.KEYUP and event.key in self.keys.values():
-                    found_key = next((key for key, value in self.keys.items() if value == event.key), None)
-                    return found_key
+            for keyvalue in range(16):
+                if self.previous_state[keyvalue] and not self.current_state[keyvalue]: # key up event
+                    return keyvalue
             return False
         
         #2. Held down
-        key_to_check = self.keys[key_hex & 0xf] # mask to limit value to 0-f
-        keys_pressed = pygame.key.get_pressed()
-        return keys_pressed[key_to_check]
+        key_to_check = key_hex & 0xf # mask to limit value to 0-f
+        return self.current_state[key_to_check]
